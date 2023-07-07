561586d84f6e5eca5d08813482a2a13a844898c7
salee2 <hleesa>
2023-07-07 21:16:11 +0900

    refactor

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -52,19 +52,19 @@ bool Fixed::operator!=(const Fixed& other) const {
 
 Fixed Fixed::operator+(const Fixed& other) const {
 	Fixed fixed;
-	fixed.setRawBits(value + other.value);
+	fixed.value = value + other.value;
 	return fixed;
 }
 
 Fixed Fixed::operator-(const Fixed& other) const {
 	Fixed fixed;
-	fixed.setRawBits(value - other.value);
+	fixed.value = value - other.value;
 	return fixed;
 }
 
 Fixed Fixed::operator*(const Fixed& other) const {
 	Fixed fixed;
-	fixed.setRawBits((long long) value * other.value >> fractionalBit);
+	fixed.value = (long long) value * other.value >> fractionalBit;
 	return fixed;
 }
 
@@ -74,7 +74,7 @@ Fixed Fixed::operator/(const Fixed& other) const {
 		exit(EXIT_FAILURE);
 	}
 	Fixed fixed;
-	fixed.setRawBits(((long long) value << fractionalBit) / other.value);
+	fixed.value = ((long long) value << fractionalBit) / other.value;
 	return fixed;
 }
 
